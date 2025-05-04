ac7b0e78f600e2bc84c35f306597822d92cce166
balt-dev <heptor42+git@gmail.com>
2025-05-04 11:21:10 -0500

    Simplify descriptions

--- localization/default.lua
+++ localization/default.lua
@@ -8,9 +8,6 @@ return {
 					"factor of #2#^X, with X",
 					"being {C:attention}randomized{} over a",
 					"standard normal distribution",
-					-- This is slightly inaccurate but I don't have a lot of space to work with :P
-					"{C:inactive}~43% chance of x1, ~16% chance of x#1# or x#2#,",
-					"{C:inactive}~0.8% chance of up to x#3# or x#4#, etc. to x0 and xInfinity",
 					"{C:inactive}More drastic multipliers are rarer"
 				},
             },
@@ -22,9 +19,6 @@ return {
 					"Values are multiplied by a factor of #2#^X",
 					"with X being {C:attention}randomized{} over a",
 					"standard normal distribution",
-					-- This is slightly inaccurate but I don't have a lot of space to work with :P
-					"{C:inactive}~43% chance of x1, ~16% chance of x#1# or x#2#,",
-					"{C:inactive}~0.8% chance of up to x#3# or x#4#, etc. to x0 and xInfinity",
 					"{C:inactive}More drastic multipliers are rarer"
 				},
             },
